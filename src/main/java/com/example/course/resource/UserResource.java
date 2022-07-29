7c96d0e95e470be96a84e00376f4ecb30f3ab0a0
João Gabriel <joaogabrielmoura14@gmail.com>
2022-07-29 18:25:13 -0300

    Atualização de usuário com PUT

--- src/main/java/com/example/course/resource/UserResource.java
+++ src/main/java/com/example/course/resource/UserResource.java
@@ -54,7 +54,16 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping(method=RequestMethod.PUT, value= "/{id}")
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDTO, @PathVariable String id){
+		User obj = service.fromDTO(objDTO);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
 	
 	
+		
 
 }
+
